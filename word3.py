f2e19e0176891aba4ea6f27228b19daf5ba6d957
fuxiaoshuai <43293026+fuxiaoshuai@users.noreply.github.com>
2018-09-18 10:40:18 +0800

    Update word3.py

--- word3.py
+++ word3.py
@@ -4,8 +4,82 @@ import os
 import random
 import shutil
 import sys
+import string
 #为打开一个相应的文件并调用shell命令
-tem = (os.popen("cat /proc/meminfo | awk -F' ' 'NR==1{ print $2 }'").read())
-返回值为1或零
-os.system()
+os.popen()  
+返回当前工作目录
+os.geetcwd()
+返回指定的路径的文件夹和文件
+os.函数用来删除一个文件:os.remove()
 
+删除多个目录：os.removedirs（r“c：\python”）
+
+检验给出的路径是否是一个文件：os.path.isfile()
+
+检验给出的路径是否是一个目录：os.path.isdir()
+
+判断是否是绝对路径：os.path.isabs()
+
+检验给出的路径是否真地存:os.path.exists()
+
+返回一个路径的目录名和文件名:os.path.split()     eg os.path.split('/home/swaroop/byte/code/poem.txt') 结果：('/home/swaroop/byte/code', 'poem.txt') 
+
+分离扩展名：os.path.splitext()
+
+获取路径名：os.path.dirname()
+
+获取文件名：os.path.basename()
+
+运行shell命令: os.system()
+
+读取和设置环境变量:os.getenv() 与os.putenv()
+
+给出当前平台使用的行终止符:os.linesep    Windows使用'\r\n'，Linux使用'\n'而Mac使用'\r'
+
+指示你正在使用的平台：os.name       对于Windows，它是'nt'，而对于Linux/Unix用户，它是'posix'
+
+重命名：os.rename（old， new）
+
+创建多级目录：os.makedirs（r“c：\python\test”）
+
+创建单个目录：os.mkdir（“test”）
+
+获取文件属性：os.stat（file）
+
+修改文件权限与时间戳：os.chmod（file）
+
+终止当前进程：os.exit（）
+
+获取文件大小：os.path.getsize（filename）
+
+
+文件操作：
+os.mknod("test.txt")        创建空文件
+fp = open("test.txt",w)     直接打开一个文件，如果文件不存在则创建文件listdir()
+目录操作：
+os.mkdir("file")                   创建目录
+复制文件：
+shutil.copyfile("oldfile","newfile")       oldfile和newfile都只能是文件
+shutil.copy("oldfile","newfile")            oldfile只能是文件夹，newfile可以是文件，也可以是目标目录
+复制文件夹：
+shutil.copytree("olddir","newdir")        olddir和newdir都只能是目录，且newdir必须不存在
+重命名文件（目录）
+os.rename("oldname","newname")       文件或目录都是使用这条命令
+移动文件（目录）
+shutil.move("oldpos","newpos")   
+删除文件
+os.remove("file")
+删除目录
+os.rmdir("dir")只能删除空目录
+shutil.rmtree("dir")    空目录、有内容的目录都可以删
+转换目录
+os.chdir("path")   换路径
+random模块
+random.randint（） random.randint(a, b)，用于生成一个指定范围内的整数。
+random.randrange 
+random.randrange([start], stop[, step])，从指定范围内，按指定基数递增的集合中 获取一个随机数。如：random.randrange(10, 100, 2)，结果相当于从[10, 12, 14, 16, ... 96, 98]序列中获取一个随机数
+random.choice 从序列中获取一个随机元素。
+sys.argv（） 模块，为位置参数模块
+sys.exit() 为中途退出程序
+string.ascii_letters()  string.ascii_lowercase和string.ascii_uppercase的综合体
+string.digits 数字（0-9）字符串
